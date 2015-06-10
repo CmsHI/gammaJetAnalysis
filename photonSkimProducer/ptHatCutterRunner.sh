b6833042bcd19193906cbff1ebdade85c9fd26c4
Kaya Tatar <tatark@mit.edu>
2015-06-10 15:29:09 -0400

    updated ptHatCutterRunner.sh

--- photonSkimProducer/ptHatCutterRunner.sh
+++ photonSkimProducer/ptHatCutterRunner.sh
@@ -7,7 +7,14 @@
 g++ ptHatCutter.C $(root-config --cflags --libs) -Wall -Wextra -g -o ptHatCutter.exe
 
 input="input.root";
-input="output.root";
+output="output.root";
 colli=2;
 ./ptHatCutter.exe "${input}" "${output}" $colli 30 50
 
+# input="/export/d00/scratch/tatar/output/merged_pPb_Pbp_MIX_AllQCDPhoton30_50_80_120_170_localJEC_v3.root";
+# output="/export/d00/scratch/tatar/output/merged_pPb_Pbp_MIX_AllQCDPhoton30_50_80_120_170_localJEC_v3";
+# colli=2;
+# ./ptHatCutter.exe "${input}" $output"_ptHat_30_50.root" $colli 30 50
+# ./ptHatCutter.exe "${input}" $output"_ptHat_50_80.root" $colli 50 80
+# ./ptHatCutter.exe "${input}" $output"_ptHat_80_120.root" $colli 80 120
+# ./ptHatCutter.exe "${input}" $output"_ptHat_120_9999.root" $colli 120 9999

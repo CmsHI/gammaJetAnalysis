6f9564b034df62c929f7222e32a5a1006c467149
Kaya Tatar <tatark@mit.edu>
2015-06-10 12:44:56 -0400

    ptHatCutter.C takes now input/output file names as argument. ptHatCutter.C is now g++ compilable.

--- photonSkimProducer/ptHatCutterRunner.sh
+++ photonSkimProducer/ptHatCutterRunner.sh
@@ -1,6 +1,13 @@
-root -l -b -q 'ptHatCutter.C+(30,50,40)'
-root -l -b -q 'ptHatCutter.C+(50,80,40)'
-root -l -b -q 'ptHatCutter.C+(80,120,40)'
-root -l -b -q 'ptHatCutter.C+(120,170,40)'
-root -l -b -q 'ptHatCutter.C+(170,9999,40)'
+#root -l -b -q 'ptHatCutter.C+(30,50,40)'
+#root -l -b -q 'ptHatCutter.C+(50,80,40)'
+#root -l -b -q 'ptHatCutter.C+(80,120,40)'
+#root -l -b -q 'ptHatCutter.C+(120,170,40)'
+#root -l -b -q 'ptHatCutter.C+(170,9999,40)'
+
+g++ ptHatCutter.C $(root-config --cflags --libs) -Wall -Wextra -g -o ptHatCutter.exe
+
+input="input.root";
+input="output.root";
+colli=2;
+./ptHatCutter.exe "${input}" "${output}" $colli 30 50
 
